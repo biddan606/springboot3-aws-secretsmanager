ef82b0487aebe86234c3261ad715af4586d6a5de
biddan606 <biddan606@gmail.com>
2024-11-21 20:06:44 +0900

    DB 연결

--- src/main/java/dev/biddan/awssecretmanager/AwsSecretmanagerApplication.java
+++ src/main/java/dev/biddan/awssecretmanager/AwsSecretmanagerApplication.java
@@ -1,7 +1,13 @@
 package dev.biddan.awssecretmanager;
 
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class AwsSecretmanagerApplication {
@@ -10,4 +16,27 @@ public class AwsSecretmanagerApplication {
         SpringApplication.run(AwsSecretmanagerApplication.class, args);
     }
 
+    @Service
+    static class DBConfigService implements CommandLineRunner {
+        private final JdbcTemplate jdbcTemplate;
+
+        @Autowired
+        public DBConfigService(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+
+        @Override
+        public void run(String... args) {
+            try {
+                System.out.println("데이터베이스 연결 테스트:");
+
+                String sql = "SELECT 1";
+                List<Map<String, Object>> results = jdbcTemplate.queryForList(sql);;
+
+                results.forEach(System.out::println);
+            } catch (Exception e) {
+                System.err.println("데이터베이스 조회 실패: " + e.getMessage());
+            }
+        }
+    }
 }
