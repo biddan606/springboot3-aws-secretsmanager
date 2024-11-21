9c823bb5e52cf163d6ab09bce74f22654fc67216
biddan606 <biddan606@gmail.com>
2024-11-21 15:54:36 +0900

    프로젝트 생성

---
+++ src/main/java/dev/biddan/awssecretmanager/AwsSecretmanagerApplication.java
@@ -0,0 +1,13 @@
+package dev.biddan.awssecretmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSecretmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSecretmanagerApplication.class, args);
+    }
+
+}
